b6fdd88f5cf4600445d765614dcedfbbf12c7c76
yoooung03 <129645571+yoooung03@users.noreply.github.com>
2025-08-22 19:06:06 +0900

    Spring Boot project setup

---
+++ src/main/java/com/eumm/dementia/dementia_support/DementiaSupportApplication.java
@@ -0,0 +1,13 @@
+package com.eumm.dementia.dementia_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DementiaSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DementiaSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eumm/dementia/dementia_support/DementiaSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eumm.dementia.dementia_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DementiaSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
